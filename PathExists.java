c260d326fcfd053ba08d1e42be0d0d477bc5a993
OgabekYuldashov <ogabek.yuldashov@gmail.com>
2019-10-17 08:20:05 -0500

    done

--- PathExists.java
+++ PathExists.java
@@ -3,18 +3,25 @@ package lab11;
 
 
 public class PathExists extends BreadthFirstSearch {
-	
+	Vertex v;
+	boolean pathExists = false;
+
 	public PathExists(Graph graph) {
 		super(graph);
 	}
 
 	//TO-DO
 	public boolean pathExists(Vertex u, Vertex v) {
+		this.v = v;
+		start(u);
+
 		return false;
 	}
 
 	@Override
 	protected void additionalProcessing() {
-		super.additionalProcessing();
+		if (hasBeenVisited(v)){
+			pathExists = true;
+		}
 	}
 }

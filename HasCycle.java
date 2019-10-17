6f8eafcceb3aa8dc0941a82138963a5fddcc870d
OgabekYuldashov <ogabek.yuldashov@gmail.com>
2019-10-16 23:58:20 -0500

    Demonstration of Graph operations using various algorithms

---
+++ HasCycle.java
@@ -0,0 +1,17 @@
+package lab11;
+
+import java.util.ArrayList;
+
+
+public class HasCycle extends BreadthFirstSearch {
+	
+	public HasCycle(Graph graph) {
+		super(graph);
+	}
+	
+	//TO-DO
+	public boolean hasCycle() {
+		return false;
+	}
+
+}
---
+++ IsConnected.java
@@ -0,0 +1,12 @@
+package lab11;
+
+public class IsConnected extends BreadthFirstSearch {
+	public IsConnected(Graph graph) {
+		super(graph);
+	}
+
+	//TO-DO
+	public boolean isConnected() {
+		return false;
+	}
+}
